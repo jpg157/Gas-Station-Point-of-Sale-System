99342b2e807cb71a8c52065f86e6ff865d133a85
jpg157 <jasonlau5000@gmail.com>
2025-03-19 17:16:18 -0700

    created models for product types

---
+++ GasStationPOS/GasStationPOS/Core/Models/Product/FuelProduct.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GasStationPOS.Core.Models.Product
+{
+
+    enum FuelType
+    {
+        GASOLINE,
+        DIESEL
+    }
+
+    class FuelProduct : Product
+    {
+        [Required]
+        public FuelType FuelType { get; set; }
+
+        [Required]
+        [Range(0.0, 10000.0, ErrorMessage = "Fuel volume must be between 0.0 and 10000.0 L")]
+        public decimal FuelVolumeLitres { get; set; }
+
+        public override string ToString()
+        {
+            string strRet = $@"
+                {base.ToString()}
+                Fuel Type: {FuelType}
+                Fuel Volume Litres: {FuelVolumeLitres}
+                ";
+            return strRet;
+        }
+    }
+}
---
+++ GasStationPOS/GasStationPOS/Core/Models/Product/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GasStationPOS.Core.Models.Product
+{
+    abstract class Product
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string ProductName { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Range(0.0, 10000.0, ErrorMessage = "Price (dollars) must be between 0.0 and 10000.0")]
+        public decimal PriceDollars { get; set; }
+
+        public override string ToString()
+        {
+            string strRet = $@"
+                Product Id: {Id}
+                Product Name: {ProductName}
+                Product Description: {Description}
+                Product PriceDollars: {PriceDollars}
+                ";
+            return strRet;
+        }
+    }
+}
